b890d17dbef59715213c98811af27fd2b00077a8
Junior Rodrigo <jvniorrodrigo@gmail.com>
2024-11-19 11:26:58 -0500

    Protección de rutas

---
+++ src/app/api/auth/[...nextauth]/pages-options.ts
@@ -0,0 +1,6 @@
+import { PagesOptions } from "next-auth";
+
+export const pagesOptions: Partial<PagesOptions> = {
+	signIn: "/auth/ingresar",
+	error: "/auth/ingresar",
+};
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,135 +1,103 @@
 "use client";
 import React, { useContext, useState } from "react";
-import {
-  FiSearch,
-  FiChevronDown,
-  FiUser,
-  FiSettings,
-  FiLogOut,
-  FiChevronsLeft,
-} from "react-icons/fi";
+import { FiSearch, FiChevronDown, FiUser, FiSettings, FiLogOut, FiChevronsLeft } from "react-icons/fi";
 import SearchModal from "./SearchModal";
 import Link from "next/link";
 import { UiContext } from "@/context/SidebarContext";
 import { UiContextType } from "@/types/SidebarOpen";
 import Image from "next/image";
+import { signOut } from "next-auth/react";
 
 const Navbar: React.FC = () => {
-  const context = useContext(UiContext) as UiContextType;
-  const [modalOpen, setModalOpen] = useState(false);
+	const context = useContext(UiContext) as UiContextType;
+	const [modalOpen, setModalOpen] = useState(false);
+	const { open, setOpen } = context;
+	const [dropdownOpen, setDropdownOpen] = useState<boolean>(false);
 
-  const toggleModal = () => {
-    setModalOpen(!modalOpen);
-  };
+	const toggleModal = () => {
+		setModalOpen(!modalOpen);
+	};
 
-  if (!context) {
-    throw new Error("useAppContext debe ser usado dentro de un UiProvider");
-  }
+	if (!context) {
+		throw new Error("useAppContext debe ser usado dentro de un UiProvider");
+	}
 
-  const { open, setOpen } = context;
+	const toggleSidebar = () => {
+		setOpen(!open);
+	};
 
-  const [dropdownOpen, setDropdownOpen] = useState(false);
+	const toggleDropdown = () => {
+		setDropdownOpen(!dropdownOpen);
+	};
 
-  const toggleSidebar = () => {
-    setOpen(!open);
-  };
+	return (
+		<nav className="bg-[#ededed] border-b border-gray-300">
+			<div className="max-h-[80px] min-h-[80px] flex items-center justify-between px-4 mx-auto">
+				{/* Icono para desplegar/ocultar sidebar */}
+				<button className={`text-3xl text-gray-400 focus:outline-none transition-transform duration-300 ${open ? "rotate-0" : "rotate-180"}`} onClick={toggleSidebar} aria-label="Toggle Sidebar">
+					<FiChevronsLeft />
+				</button>
 
-  const toggleDropdown = () => {
-    setDropdownOpen(!dropdownOpen);
-  };
+				{/* Barra de búsqueda */}
+				<div className="flex items-center flex-grow px-4">
+					<button className="text-gray-300 hover:text-white md:hidden text-2xl" onClick={toggleModal} aria-label="Open Search">
+						<FiSearch />
+					</button>
+					<div className="relative hidden md:flex w-96">
+						<input type="text" placeholder="Buscar..." className="w-full bg-slate-50 rounded-md py-2 px-4 focus:outline-none focus:ring-0 placeholder-gray-400 border border-gray-400" />
+						<div className="absolute inset-y-0 right-0 flex items-center pr-3">
+							<FiSearch className="text-gray-400 text-xl" />
+						</div>
+					</div>
 
-  return (
-    <nav className="bg-[#ededed] border-b border-gray-300">
-      <div className="max-h-[80px] min-h-[80px] flex items-center justify-between px-4 mx-auto">
-        {/* Icono para desplegar/ocultar sidebar */}
-        <button
-          className={`text-3xl text-gray-400 focus:outline-none transition-transform duration-300 ${
-            open ? "rotate-0" : "rotate-180"
-          }`}
-          onClick={toggleSidebar}
-          aria-label="Toggle Sidebar"
-        >
-          <FiChevronsLeft />
-        </button>
+					<SearchModal isOpen={modalOpen} onClose={toggleModal} />
+				</div>
 
-        {/* Barra de búsqueda */}
-        <div className="flex items-center flex-grow px-4">
-          <button
-            className="text-gray-300 hover:text-white md:hidden text-2xl"
-            onClick={toggleModal}
-            aria-label="Open Search"
-          >
-            <FiSearch />
-          </button>
-          <div className="relative hidden md:flex w-96">
-            <input
-              type="text"
-              placeholder="Buscar..."
-              className="w-full bg-slate-50 rounded-md py-2 px-4 focus:outline-none focus:ring-0 placeholder-gray-400 border border-gray-400"
-            />
-            <div className="absolute inset-y-0 right-0 flex items-center pr-3">
-              <FiSearch className="text-gray-400 text-xl" />
-            </div>
-          </div>
-          <SearchModal isOpen={modalOpen} onClose={toggleModal} />
-        </div>
+				{/* Sección de usuario */}
+				<div className="flex items-center space-x-4 relative">
+					{/* Botón de usuario que muestra el dropdown */}
+					{/*TODO: Corregir error button aria-expanded es una expresión */}
+					<button type="button" className="flex items-center focus:outline-none space-x-2" onClick={toggleDropdown} aria-haspopup="true" aria-expanded={dropdownOpen}>
+						<div className="text-left">
+							<p className="text-base font-medium">Juan Carranza</p>
+							<p className="text-sm text-gray-400 text-right">Peru</p>
+						</div>
+						<Image src="/images/login.png" alt="Profile" className="w-10 h-10 rounded-full" height={40} width={40} />
+						<FiChevronDown className={`text-xl ml-1 transition-transform duration-300 ${dropdownOpen ? "rotate-180" : "rotate-0"}`} />
+					</button>
 
-        {/* Sección de usuario */}
-        <div className="flex items-center space-x-4 relative">
-          {/* Botón de usuario que muestra el dropdown */}
-          <button
-            className="flex items-center focus:outline-none space-x-2"
-            onClick={toggleDropdown}
-            aria-haspopup="true"
-            aria-expanded={dropdownOpen}
-          >
-            <div className="text-left">
-              <p className="text-base font-medium">Juan Carranza</p>
-              <p className="text-sm text-gray-400 text-right">Peru</p>
-            </div>
-            <Image
-              src="/images/login.png"
-              alt="Profile"
-              className="w-10 h-10 rounded-full"
-              height={40}
-              width={40}
-            />
-            <FiChevronDown
-              className={`text-xl ml-1 transition-transform duration-300 ${
-                dropdownOpen ? "rotate-180" : "rotate-0"
-              }`}
-            />
-          </button>
-
-          {/* Dropdown para el usuario */}
-          {dropdownOpen && (
-            <div className="absolute right-0 mt-52 w-48 bg-[#ededed] shadow-lg z-10">
-              <ul>
-                <li className="hover:bg-gray-300">
-                  <Link href="#" className="flex items-center p-3">
-                    <FiUser className="mr-2 text-xl" />
-                    Perfil
-                  </Link>
-                </li>
-                <li className="hover:bg-gray-300">
-                  <Link href="#" className="flex items-center p-3">
-                    <FiSettings className="mr-2 text-xl" />
-                    Configuraciones
-                  </Link>
-                </li>
-                <li className="hover:bg-gray-300">
-                  <button className="flex items-center p-3 w-full text-left">
-                    <FiLogOut className="mr-2 text-xl" />
-                    Salir
-                  </button>
-                </li>
-              </ul>
-            </div>
-          )}
-        </div>
-      </div>
-    </nav>
-  );
+					{/* Dropdown para el usuario */}
+					{dropdownOpen && (
+						<div className="absolute right-0 mt-52 w-48 bg-[#ededed] shadow-lg z-10">
+							<ul>
+								<li className="hover:bg-gray-300">
+									<Link href="#" className="flex items-center p-3">
+										<FiUser className="mr-2 text-xl" />
+										Perfil
+									</Link>
+								</li>
+								<li className="hover:bg-gray-300">
+									<Link href="#" className="flex items-center p-3">
+										<FiSettings className="mr-2 text-xl" />
+										Configuraciones
+									</Link>
+								</li>
+								<li className="hover:bg-gray-300">
+									<button
+										onClick={() => signOut({ callbackUrl: "/auth/ingresar" })} // Redirige al login después de cerrar sesión
+										className="flex items-center p-3 w-full text-left"
+									>
+										<FiLogOut className="mr-2 text-xl" />
+										Salir
+									</button>
+								</li>
+							</ul>
+						</div>
+					)}
+				</div>
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
