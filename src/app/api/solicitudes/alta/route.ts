cf49683e55e8501b98dadb65f82b118164045f6b
Junior Rodrigo <jvniorrodrigo@gmail.com>
2024-11-20 15:44:47 -0500

    Rutas de API Funcionando correctamente

--- src/app/api/solicitudes/alta/route.ts
+++ src/app/api/solicitudes/alta/route.ts
@@ -8,7 +8,14 @@ export async function GET() {
 // Manejo del método POST
 export async function POST(request: Request) {
 	try {
+		/* 
+		1. Se reciben los parámetros
+		2. Se almacenan en la db
+		3. Se envía correo electrónico
+		*/
+
 		const data = await request.json();
+		console.log(data);
 		return NextResponse.json({ message: "POST request successful", data });
 	} catch (error) {
 		console.error("Error processing POST:", error);
