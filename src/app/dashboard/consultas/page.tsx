80e66f0ab5d4e48b49ee2a13694e103ce34e0b09
IvyedSG <deyvioscarzanches@gmail.com>
2024-11-26 22:26:43 -0500

    mejora page consulta

--- src/app/dashboard/consultas/page.tsx
+++ src/app/dashboard/consultas/page.tsx
@@ -121,6 +121,7 @@ const Page: React.FC = () => {
     setSelectedArea("");
   };
 
+  
   return (
     <div className="p-6 bg-gray-50 min-h-screen">
       <h1 className="text-2xl font-bold mb-6">Consultas</h1>
