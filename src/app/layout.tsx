cfb92baffdeaf2bfcfaa3fd42bed9668a11334db
IvyedSG <deyvioscarzanches@gmail.com>
2024-11-27 14:49:20 -0500

    mejora de type user y consultas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,36 @@
+// app/layout.tsx
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
+import localFont from "next/font/local";
+import SessionWrapper from "@/components/SessionWrapper";
 
 const geistSans = localFont({
-	src: "./fonts/GeistVF.woff",
-	variable: "--font-geist-sans",
-	weight: "100 900",
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
 });
 const geistMono = localFont({
-	src: "./fonts/GeistMonoVF.woff",
-	variable: "--font-geist-mono",
-	weight: "100 900",
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
 });
 
 export const metadata: Metadata = {
-	title: "Sistema de Forzados",
-	description: "Sistema de Forzados",
+  title: "Sistema de Forzados",
+  description: "Sistema de Forzados",
 };
 
 export default function RootLayout({
-	children,
-}: Readonly<{
-	children: React.ReactNode;
-}>) {
-	return (
-		<html lang="en">
-			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
-		</html>
-	);
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        {/* Envolver el contenido con el SessionWrapper */}
+        <SessionWrapper>{children}</SessionWrapper>
+      </body>
+    </html>
+  );
 }
---
+++ src/types/UserSession.ts
@@ -0,0 +1,8 @@
+
+export type UserSession = {
+    id: number; // ID del usuario
+    name: string; // Nombre del usuario
+    email: string; // Correo del usuario
+    area: string; // Área de trabajo del usuario
+  };
+  
