7210b25560f558a8e65a23c1f8312fbc3dc1ea2b
Junior Rodrigo <jvniorrodrigo@gmail.com>
2024-12-03 16:27:29 -0500

    Página temporal de estadísticas

---
+++ src/app/dashboard/estadisticas/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Page: React.FC = () => {
+	return (
+		<div>
+			<h1>Estadísticas</h1>
+			<p>En esta página se mostrarán las estadísticas de la aplicación.</p>
+		</div>
+	);
+};
+
+export default Page;
