e07e8274b9217d97f27fbe8279d831393895bd99
Junior Rodrigo <jvniorrodrigo@gmail.com>
2024-12-04 09:34:28 -0500

    User Update

--- src/app/api/usuarios/route.ts
+++ src/app/api/usuarios/route.ts
@@ -5,12 +5,42 @@ import { poolPromise } from "@sql/lib/db";
 export async function GET() {
 	try {
 		const pool = await poolPromise;
-		const { recordset } = await pool.request().query("SELECT USUARIO_ID, NOMBRE,APEPATERNO, APEMATERNO FROM MAE_USUARIO");
+		const { recordset } = await pool.request().query(`SELECT UX.USUARIO_ID,
+		UX.NOMBRE,
+		UX.APEPATERNO,
+		UX.APEMATERNO,
+		UX.AREA_ID,
+		AR.DESCRIPCION AS AREA_DESCRIPCION,
+		UX.ROL_ID,
+		ROL.DESCRIPCION,
+		UX.ESTADO,
+		UX.DNI,
+		UX.PUESTO_ID,
+		PX.DESCRIPCION,
+		UX.CORREO,
+		UX.USUARIO
+
+	FROM MAE_USUARIO UX
+		LEFT JOIN MAE_AREA AR ON UX.AREA_ID = AR.AREA_ID
+		LEFT JOIN MAE_ROL ROL ON UX.ROL_ID = ROL.ROL_ID
+		LEFT JOIN MAE_PUESTO PX ON UX.PUESTO_ID = PX.PUESTO_ID`);
 
 		const turnos = recordset.map((singleValue) => {
 			return {
 				id: singleValue.USUARIO_ID,
-				nombre: singleValue.NOMBRE + " " + singleValue.APEPATERNO + " " + singleValue.APEMATERNO,
+				nombre: singleValue.NOMBRE,
+				apePaterno: singleValue.APEPATERNO,
+				apeMaterno: singleValue.APEMATERNO,
+				areaId: singleValue.AREA_ID,
+				areaDescripcion: singleValue.AREA_DESCRIPCION,
+				rolId: singleValue.ROL_ID,
+				rolDescripcion: singleValue.DESCRIPCION,
+				estado: singleValue.ESTADO,
+				dni: singleValue.DNI,
+				puestoId: singleValue.PUESTO_ID,
+				puestoDescripcion: singleValue.DESCRIPCION,
+				correo: singleValue.CORREO,
+				usuario: singleValue.USUARIO,
 			};
 		});
 		return NextResponse.json({ success: true, values: turnos });
@@ -97,5 +127,26 @@ export async function PUT(request: Request) {
 	} catch (error) {
 		console.error("Error processing PUT:", error);
 		return NextResponse.json({ success: false, message: "Error updating data" }, { status: 500 });
+	}
+}
+
+export async function DELETE(request: Request) {
+	try {
+		const pool = await poolPromise;
+		const { usuarioId, usuarioModificacion } = await request.json();
+		const result = await pool.request().input("usuarioId", usuarioId).input("usuarioModificacion", usuarioModificacion).query(`UPDATE MAE_USUARIO SET 
+			        ESTADO = 0, 
+			        USUARIO_MODIFICACION = @usuarioModificacion, 
+			        FECHA_MODIFICACION = GETDATE() 
+			        WHERE USUARIO_ID = @usuarioId`);
+
+		if (result.rowsAffected[0] > 0) {
+			return NextResponse.json({ success: true, message: "user status updated to 0" });
+		} else {
+			return NextResponse.json({ success: false, message: "no user status updated" }, { status: 500 });
+		}
+	} catch (error) {
+		console.error("Error processing DELETE:", error);
+		return NextResponse.json({ success: false, message: "Error updating user status" }, { status: 500 });
 	}
 }
